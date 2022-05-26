14ac30e454314591cffdd76c86b4ed64f50dc0bb
vitorcorrea18 <vitorcorrea18@gmail.com>
2022-05-26 19:17:37 -0400

    feat: Requisito 2 - Cria a model User (done)

---
+++ src/database/models/user.js
@@ -0,0 +1,14 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+      id: DataTypes.INTEGER,
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING
+    }, {
+      timestamps: false
+    });
+  return User;
+};
+
+module.exports = User;
