1942b114be9da68027368cdd5a9e7d184c823180
VitorCorrea18 <vitorcorrea18@gmail.com>
2022-05-28 13:50:57 -0400

    fix: correção lint

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const controller = require('../controllers');
-const middlewares = require('../middlewares/');
+const middlewares = require('../middlewares');
 
 router.post('/', middlewares.validateLogin, controller.login);
 
